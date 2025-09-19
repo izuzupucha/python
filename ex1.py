6a6a4accc2e227ec4044049cf92eb8c6e68c10a3
izuzupucha <izuzupucha@gmail.com>
2025-09-19 22:30:16 +0700

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,16 @@
+print('%r %r' %('Kteam', 15))
+lname = "David";
+fname = "IDK";
+print("My name is %s" % lname);
+print("%s %r" % (fname, lname));
+a = 10;
+b = 2.6;
+c = a//b;
+print(c);
+
+a = 3 
+b = 4
+result = -2<= a < 5 < b != 8
+print(result)
+
+def f(*,a) : print(a)
